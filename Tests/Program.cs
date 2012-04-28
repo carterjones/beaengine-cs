eb67a487e411d1a65d6896358fe2222c7f7ada48
Carter Jones <carter@carterjones.info>
2012-04-28 15:13:07 -0700

    use a simple test framework

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -4,15 +4,64 @@ namespace Tests
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using System.Runtime.InteropServices;
+    using System.Text;
     using BeaEngineCS;
 
-    class Program
+    /// <summary>
+    /// Tests various functionality of the distorm3cs interface.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Runs the collection of tests of the BeaEngineCS interface.
+        /// </summary>
+        /// <param name="args">Command line arguments passed to the program.</param>
+        public static void Main(string[] args)
         {
-            int dataSize = 0x1000;
+            bool result = true;
+            bool tmpResult = false;
+
+            result &= tmpResult = Program.VersionTest();
+            Console.WriteLine("VersionTest():  " + (tmpResult ? "Passed" : "Failed"));
+            result &= tmpResult = Program.RevisionTest();
+            Console.WriteLine("RevisionTest(): " + (tmpResult ? "Passed" : "Failed"));
+            result &= tmpResult = Program.DisasmTest();
+            Console.WriteLine("DisasmTest():   " + (tmpResult ? "Passed" : "Failed"));
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("End result:     " + (result ? "All passed" : "Not all passed"));
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Tests the BeaEngineCS.BeaEngineVersion() function.
+        /// </summary>
+        /// <returns>Returns true if the test passed.</returns>
+        private static bool VersionTest()
+        {
+            return BeaEngine.Version.Equals("4.1");
+        }
+
+        /// <summary>
+        /// Tests the BeaEngineCS.BeaEngineRevision() function.
+        /// </summary>
+        /// <returns>Returns true if the test passed.</returns>
+        private static bool RevisionTest()
+        {
+            return BeaEngine.Revision.Equals("172");
+        }
+
+        /// <summary>
+        /// Tests the BeaEngineCS.Disasm() function.
+        /// </summary>
+        /// <returns>Returns true if the test passed.</returns>
+        private static bool DisasmTest()
+        {
+            int dataSize = 0x100;
             IntPtr data = Marshal.AllocHGlobal(dataSize);
             for (int i = 0; i < dataSize; ++i)
             {
@@ -25,14 +74,16 @@ namespace Tests
             if (len == BeaEngine.UnknownOpcode)
             {
                 Console.Error.WriteLine("Unknown opcode.");
+                return false;
             }
             else if (len == BeaEngine.OutOfRange)
             {
                 Console.Error.WriteLine("Out of range.");
+                return false;
             }
             else
             {
-                Console.WriteLine(inst.CompleteInstr);
+                return inst.CompleteInstr.Equals("add byte ptr [eax], al");
             }
         }
     }
